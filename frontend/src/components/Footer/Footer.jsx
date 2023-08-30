55c347d41d19de630a45c659df6553883fe4a967
HM-Mphayi <hm.mphayi11@gmail.com>
2023-08-30 19:55:16 +0200

    Modified the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,73 +1,82 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaTwitter, FaInstagramSquare } from "react-icons/fa";
+import { AiFillCopyrightCircle } from "react-icons/ai";
 import "./Footer.scss";
 
 function Footer() {
   return (
     <section className="main-footer">
       <div className="wrapper">
-        <div className="top">
-          <div className="col">
-            <h3>CATEGORIES</h3>
-            <div>
-              <Link className="custom-link" to={"products/category/Men"}>
-                MEN
-              </Link>
+        <div className="top d-flex flex-column flex-sm-row">
+          <section className=" d-flex gap-0 gap-sm-5 me-0 me-sm-5">
+            <div className="me-5">
+              <h3>CATEGORIES</h3>
+              <div>
+                <Link className="custom-link" to={"products/category/Men"}>
+                  MEN
+                </Link>
+              </div>
+              <div>
+                <Link className="custom-link" to={"products/category/Kids"}>
+                  KIDS
+                </Link>
+              </div>
+              <div>
+                <Link className="custom-link" to={"products/category/Ladies"}>
+                  WOMEN
+                </Link>
+              </div>
             </div>
-            <div>
-              <Link className="custom-link" to={"products/category/Kids"}>
-                KIDS
-              </Link>
+            <div className="me-5">
+              <h3>LINKS</h3>
+              <div>
+                <Link className="custom-link">FAQ</Link>
+              </div>
+              <div>
+                <Link className="custom-link">PAGES</Link>
+              </div>
+              <div>
+                <Link className="custom-link">STORES</Link>
+              </div>
             </div>
-            <div>
-              <Link className="custom-link" to={"products/category/Ladies"}>
-                WOMEN
-              </Link>
-            </div>
-          </div>
-          <div className="col">
-            <h3>LINKS</h3>
-            <div>
-              <Link className="custom-link">FAQ</Link>
-            </div>
-            <div>
-              <Link className="custom-link">PAGES</Link>
-            </div>
-            <div>
-              <Link className="custom-link">STORES</Link>
-            </div>
-          </div>
+          </section>
 
-          <div className="col" id="about">
-            <h3>ABOUT</h3>
+          <section className="d-flex flex-column flex-sm-row gap-5  ms-0 ms-sm-5">
+            <div>
+              <h3 className="mt-3 mt-sm-0">ABOUT</h3>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-          <div className="col">
-            <h3>CONTACT</h3>
-
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div className="contact">
+              <h3>CONTACT</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </section>
         </div>
 
-        <div className="bottom">
-          <div>
-            <FaFacebookF />
+        <section className="bottom d-flex flex-column">
+          <div className="d-flex gap-4">
+            <div>
+              <FaFacebookF />
+            </div>
+            <div>
+              <FaInstagramSquare />
+            </div>
+            <div>
+              <FaTwitter />
+            </div>
           </div>
-          <div>
-            <FaInstagramSquare />
+          <div className="copyright mt-2">
+            <span>&copy; 2023 Swanky Styles</span>
           </div>
-          <div>
-            <FaTwitter />
-          </div>
-        </div>
+        </section>
       </div>
     </section>
   );
