90aa5ee26e1882ad2f7bacdaab1bfedb119605f6
HM-Mphayi <hm.mphayi11@gmail.com>
2023-08-31 10:28:37 +0200

    modified the contatct and about section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaTwitter, FaInstagramSquare } from "react-icons/fa";
-import { AiFillCopyrightCircle } from "react-icons/ai";
 import "./Footer.scss";
 
 function Footer() {
   return (
     <section className="main-footer">
       <div className="wrapper">
-        <div className="top d-flex flex-column flex-sm-row">
+        <div className="top d-flex flex-column flex-lg-row">
           <section className=" d-flex gap-0 gap-sm-5 me-0 me-sm-5">
-            <div className="me-5">
+            <div className="me-5 me-sm-5">
               <h3>CATEGORIES</h3>
               <div>
                 <Link className="custom-link" to={"products/category/Men"}>
@@ -28,7 +27,7 @@ function Footer() {
                 </Link>
               </div>
             </div>
-            <div className="me-5">
+            <div className="me-0 me-sm-5">
               <h3>LINKS</h3>
               <div>
                 <Link className="custom-link">FAQ</Link>
@@ -42,21 +41,24 @@ function Footer() {
             </div>
           </section>
 
-          <section className="d-flex flex-column flex-sm-row gap-5  ms-0 ms-sm-5">
-            <div>
-              <h3 className="mt-3 mt-sm-0">ABOUT</h3>
+          <section className="d-flex flex-column flex-md-row gap-5  ms-0 ms-lg-5">
+            <div className="about">
+              <h3 className="mt-3 mt-lg-0">ABOUT</h3>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Discover the latest fashion trends at Swanky Styles. Shop our
+                curated collection of stylish clothing items to elevate your
+                wardrobe. Find the perfect look for every occasion and embrace
+                your unique style with us today!
               </p>
             </div>
             <div className="contact">
               <h3>CONTACT</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+              <ul>
+                <li>Address: 123 Main Street, City, State, Zip Code</li>
+                <li>Phone: (123) 456-7890</li>
+                <li>Email: support@swankystyles.com</li>
+              </ul>
             </div>
           </section>
         </div>
